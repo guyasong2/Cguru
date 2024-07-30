40f4213bbe3455cef704807c4151cf2d92336c05
Cguru <103155655+guyasong2@users.noreply.github.com>
2024-07-30 13:38:09 +0100

    Update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,16 +5,11 @@ import me from "@/public/me.png";
 const Hero = () => {
   return (
     <div className="md:flex-row md:flex w-[80%] md:w-[56%] mx-auto justify-between pt-20 md:pt-0">
-      <Image alt="me"
-        className="rounded-full"
-        src={me}
-        width={400}
-        height={400}
-      />
-
+      <Image alt="me" className="rounded-full" src={me} width={400} height={400}/>
+      
       <div className="text-center md:pt-16 pt-10  md:ml-24 ml-0">
         <h2 className="uppercase text-3xl mb-2 text-white">
-          I'm <span className="text-black font-bold"> Guy Asong</span>
+          I &rsquo m <span className="text-black font-bold"> Guy Asong</span>
         </h2>
         <h3 className="text-xl text-white">FRONTEND DEVELOPER</h3>
         <hr className="mb-3"/>
