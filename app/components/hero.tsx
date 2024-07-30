60a33d1db3eba152c9947a87695035812f257b0a
Cguru <103155655+guyasong2@users.noreply.github.com>
2024-07-30 14:17:50 +0100

    Update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,7 +5,7 @@ import me from "@/public/me.png";
 const Hero = () => {
   return (
     <div className="md:flex-row md:flex w-[80%] md:w-[60%] mx-auto justify-between pt-20 md:pt-0">
-      <Image alt="me" className="rounded-full" src={me} width={400} height={400}/>
+      <Image alt="me" className="rounded-full" src={me} width={300} height={300}/>
       
       <div className="text-center md:pt-16 pt-10  md:ml-24 ml-0">
         <h2 className="uppercase text-3xl mb-2 text-white">
